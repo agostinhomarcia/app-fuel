041bc1a0e39cd939fdd5d47994fbaf2c9d14e3fa
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2023-12-16 14:25:38 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,176 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, Modal, Image, StyleSheet, StatusBar } from 'react-native';
+import RNModal from 'react-native-modal';
 
-export default function App() {
+const App = () => {
+  const [alcool, setAlcool] = useState('');
+  const [gasolina, setGasolina] = useState('');
+  const [resultado, setResultado] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const calcularRecomendacao = () => {
+    const precoAlcool = parseFloat(alcool);
+    const precoGasolina = parseFloat(gasolina);
+
+    if (isNaN(precoAlcool) || isNaN(precoGasolina)) {
+      setResultado('Insira valores válidos para Álcool e Gasolina.');
+      return;
+    }
+
+    const resultadoCalculo = precoAlcool / precoGasolina;
+
+    if (resultadoCalculo < 0.7) {
+      setResultado('Recomendação: Abasteça com Álcool.');
+      setModalVisible(true);
+    } else {
+      setResultado('Recomendação: Abasteça com Gasolina.');
+    }
+  };
+
+  const resetarValores = () => {
+    setAlcool('');
+    setGasolina('');
+    setResultado('');
+    setModalVisible(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <StatusBar backgroundColor="#ccc" barStyle="light-content" />
+
+      <Image
+        source={require('./src/assets/logo.png')}
+        style={styles.imagemPaginaInicial}
+      />
+
+      <Text style={styles.label}>Preço do Álcool (R$):</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o preço do Álcool"
+        keyboardType="numeric"
+        value={alcool}
+        onChangeText={(text) => setAlcool(text)}
+      />
+
+      <Text style={styles.label}>Preço da Gasolina (R$):</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite o preço da Gasolina"
+        keyboardType="numeric"
+        value={gasolina}
+        onChangeText={(text) => setGasolina(text)}
+      />
+
+      <Button title="Calcular " onPress={calcularRecomendacao} />
+
+      <Text style={styles.resultado}>{resultado}</Text>
+
+      <RNModal isVisible={modalVisible} style={styles.modal}>
+        <View style={styles.modalContainer}>
+
+          <Image
+            source={require('./src/assets/gas.png')}
+            style={styles.imagemModal}
+          />
+
+          <Text style={styles.modalTexto}>Compensa usar álcool!</Text>
+
+
+          <Text style={styles.label}>Novo preço do Álcool (R$):</Text>
+          <TextInput
+            style={styles.modalInput}
+            placeholder="Digite o preço do Álcool"
+            keyboardType="numeric"
+            value={alcool}
+            onChangeText={(text) => setAlcool(text)}
+          />
+
+          <Text style={styles.label}>Novo preço da Gasolina (R$):</Text>
+          <TextInput
+            style={styles.modalInput}
+            placeholder="Digite o preço da Gasolina"
+            keyboardType="numeric"
+            value={gasolina}
+            onChangeText={(text) => setGasolina(text)}
+          />
+
+
+
+          <Button
+            title="Fechar"
+            onPress={() => {
+              resetarValores();
+            }}
+          />
+        </View>
+      </RNModal>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    padding: 20,
     alignItems: 'center',
+    backgroundColor: '#ccc',
+    flex: 1,
+  },
+  imagemPaginaInicial: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  label: {
+    marginTop: 10,
+    fontSize: 16,
+    color: '#333',
+  },
+  input: {
+    height: 40,
+    borderColor: '#333',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+    width: '100%',
+  },
+  resultado: {
+    marginTop: 20,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  modal: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    width: '80%',
+    padding: 20,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  imagemModal: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+  },
+  modalTexto: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333',
+  },
+  modalInput: {
+    height: 40,
+    borderColor: '#333',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+    width: '100%',
   },
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,129 @@
+<p align="center">
+   <img src="https://media.giphy.com/media/UTjIGZdweJ3kWgPhvo/giphy.gif" alt="fuel" width="280"/>
+</p>
+
+<p align="right">
+   <img src="https://media.giphy.com/media/QuyQeCDS6PLCP9iAOW/giphy.gif" alt="fuel" width="130"/>
+</p>
+
+<h1 align="center"></h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/agostinhomarcia/app-movies?color=FF0000">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/agostinhomarcia/app-movies?color=FF0000">
+
+  <img alt="License" src="https://img.shields.io/github/license/agostinhomarcia/app-movies?color=FF0000">
+
+   <img alt="Github issues" src="https://img.shields.io/github/issues/agostinhomarcia/app-movies?color=FF0000" />
+
+   <img alt="Github forks" src="https://img.shields.io/github/forks/agostinhomarcia/app-movies?color=FF0000" />
+
+   <img alt="Github stars" src="https://img.shields.io/github/stars/agostinhomarcia/app-movies?color=FF0000" /> 
+</p>
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0; 
+  <a href="#sparkles-features">Features</a> &#xa0; | &#xa0;
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="#memo-license">License</a> &#xa0; | &#xa0;
+  <a href="https://github.com/agostinhomarcia" target="_blank">Author</a>&#xa0; | &#xa0
+  <a href="#" target="_blank" rel="noopener noreferrer">Projeto</a>
+</p>
+
+<br>
+
+## :dart: About
+
+<h4 align="center">Lista de Filmes </h4>
+
+<p align="center">
+  <img src="assets/fuel.gif" alt="teste-morty" />
+  
+</p>
+
+<p align="left">
+
+### Álcool ou Gasolina? - Calculadora de Abastecimento em React Native
+
+Este projeto é uma aplicação simples em React Native que ajuda os usuários a decidirem se é mais vantajoso abastecer com álcool ou gasolina, com base nos preços informados. A calculadora compara o valor do litro do álcool com o da gasolina e fornece uma recomendação com base na relação entre esses preços.
+
+##### Recursos Principais:
+
+Interface intuitiva para inserção de preços de álcool e gasolina.
+Cálculo automático da relação entre os preços.
+Recomendação de abastecimento com base no cálculo.
+Modal informativo indicando a recomendação.
+
+##### Instruções de Uso:
+
+Insira o preço do litro do álcool no campo correspondente.
+Insira o preço do litro da gasolina no campo correspondente.
+Clique em "Calcular Recomendação".
+Visualize a recomendação na tela principal.
+Em caso de recomendação para abastecer com álcool, o modal informativo será exibido, permitindo a inserção de novos valores e recálculo.
+
+###### Tecnologias Utilizadas:
+
+React Native
+React Navigation
+React Native Modal
+StyleSheet (Estilização)
+Componentes Nativos do React Native (Image, TextInput, Button, Modal, StatusBar, etc.)
+
+##### Como Executar o Projeto:
+
+Clone este repositório.
+Instale as dependências usando `npm install` ou `yarn install` .
+Execute o aplicativo em um emulador ou dispositivo usando npm run android ou npm run ios (ou equivalentes do Yarn).
+
+</p>
+
+## :sparkles: Features
+
+:heavy_check_mark: Feature 1;\
+:heavy_check_mark: Feature 2;\
+:heavy_check_mark: Feature 3;
+
+## :rocket: Technologies
+
+The following tools were used in this project:
+
+- [React-Native](https://reactnative.dev/docs/getting-started)
+- [React.js](https://legacy.reactjs.org/)
+- [CSS](https://developer.mozilla.org/pt-BR/docs/Web/CSS)
+- [Axios](https://axios-http.com/docs/intro)
+
+## :white_check_mark: Requirements
+
+Before starting :checkered_flag:, you need to have [Git](https://git-scm.com) and [Node](https://nodejs.org/en/) installed.
+
+## :checkered_flag: Starting
+
+```bash
+# Clone this project
+$ git clone https://github.com/agostinhomarcia/app-movies.git
+# Access
+$ cd app-movies
+# Install dependencies
+$ yarn ou npm install
+# Run the project
+$ npx expo start
+# The server will initialize in the <exp://192.168.15.2:8081>
+```
+
+## :memo: License
+
+This project is under the [MIT license](./License).
+
+Made with love by [Márcia Agostinho](https://github.com/agostinhomarcia) 🚀.
+
+<p align="center">
+   <img src="https://media.giphy.com/media/Yg8vGWejajI3ZYywkl/giphy.gif" alt="fuel" width="230"/>
+</p>
+
+&#xa0;
+
+<a href="#top">Back to top </a>
